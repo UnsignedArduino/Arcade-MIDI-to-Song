005fd6bad4ac9068cca119663aa8f62fac18c145
UnsignedArduino <UnsignedArduino@outlook.com>
2023-02-02 16:28:22 -0500

    Arguments

--- src/main.py
+++ src/main.py
@@ -1,3 +1,5 @@
+from pathlib import Path
+from typing import Optional
 from argparse import ArgumentParser
 from utils.logger import create_logger
 import logging
@@ -7,5 +9,12 @@ logger = create_logger(name=__name__, level=logging.DEBUG)
 parser = ArgumentParser(prog="ArcadeMIDItoSong",
                         description="A program to convert MIDI files to the "
                                     "Arcade song format. ")
+parser.add_argument("--input", "-i", required=True, type=Path,
+                    help="Input MIDI file")
+parser.add_argument("--output", "-o", type=Optional[Path],
+                    help="Output text file path, otherwise we will output to "
+                         "standard output.")
 args = parser.parse_args()
 logger.debug(f"Received arguments: {args}")
+
+
