e749ec4c1b90904df4729fb11a0f276fea7ce645
UnsignedArduino <UnsignedArduino@outlook.com>
2023-02-02 16:25:43 -0500

    Argparser

--- src/main.py
+++ src/main.py
@@ -1,4 +1,11 @@
+from argparse import ArgumentParser
 from utils.logger import create_logger
 import logging
 
 logger = create_logger(name=__name__, level=logging.DEBUG)
+
+parser = ArgumentParser(prog="ArcadeMIDItoSong",
+                        description="A program to convert MIDI files to the "
+                                    "Arcade song format. ")
+args = parser.parse_args()
+logger.debug(f"Received arguments: {args}")
